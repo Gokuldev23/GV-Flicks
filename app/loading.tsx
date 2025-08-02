f57c01a8a68d62c501d13295f3270583ecfec1a5
Gokuldev23 <gkforgeneral@gmail.com>
2025-08-02 21:24:28 +0530

    feat: loading skeleton added

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MovieLoading from "./lib/components/MovieLoading"
+
+export default function Loading() {
+  return (
+    <div className='p-4'>
+        <MovieLoading/>
+    </div>
+  )
+}
